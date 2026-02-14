bbab5d00c99c2594a7aa284c17718b7f3a168d43
Telli Sow-Mouvagha <tellisowmouvagha@gmail.com>
2026-02-14 21:02:02 +0100

    Ajout de "features" et intégration de Tailwind CSS dedans

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import { Button } from "@/components/ui/button"
+import { TestComponent } from "@/features/test"
 
 export default function Home() {
   return (
     <div>
-      <Button size="sm">Click me</Button>
-      <p className="text-blue-600 font-semibold" >
-        Text
-      </p>
+      <TestComponent></TestComponent>
     </div>
   )
 }
---
+++ src/features/test.tsx
@@ -0,0 +1,7 @@
+export const TestComponent = () => {
+    return(
+        <div className="text-red-500">
+            Hello test component
+        </div>
+    );
+};
