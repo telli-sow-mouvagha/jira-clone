5bd5887dc60077cba396c7f39737a2dadb4f645e
Telli Sow-Mouvagha <tellisowmouvagha@gmail.com>
2026-03-04 14:49:40 +0100

    some more tests

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,27 @@ import { TestComponent } from "@/features/test"
 
 export default function Home() {
   return (
-    <div>
+    <div className="">
       <Button>
         Primary
+      </Button>
+      <Button variant="secondary">
+        Secondary
+      </Button>
+      <Button variant="destructive">
+        Destructive
+      </Button>
+      <Button variant="ghost">
+        Ghost
+      </Button>
+      <Button variant="muted">
+        Muted
+      </Button>
+      <Button variant="outline">
+        Outline
+      </Button>
+      <Button variant="teritary">
+        Teritary
       </Button>
     </div>
   )
